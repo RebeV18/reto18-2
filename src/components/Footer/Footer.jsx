f9b9b7393d44b040e2c8c5ed1bb1208c46e2731b
Rebeca <inscripciones225@gmail.com>
2025-02-27 00:35:25 -0300

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,8 @@
-const Footer = () => {
-  const styles = {
-    footer: {
-      backgroundColor: '#0E0D0D',
-      color: '#fff',
-      textAlign: 'center',
-      padding: '10px 0',
-      //position: 'fixed',
-      left: '0',
-      bottom: '0',
-      width: '100%',
-      marginTop: '20px',
-    },
-    link: {
-      color: '#fff',
-      textDecoration: 'none',
-      margin: '0 10px',
-    },
-  };
+import './Footer.css';
 
+const Footer = () => {
   return (
-    <footer style={styles.footer}>
+    <footer>
       <p>&copy; 2025 Mi Sitio de Películas. Todos los derechos reservados. Uso exclusivo para fines educativos.</p>
     </footer>
   );
